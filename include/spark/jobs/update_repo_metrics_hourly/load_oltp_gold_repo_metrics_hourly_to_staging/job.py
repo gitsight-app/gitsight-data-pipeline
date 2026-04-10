f88a04d39574d52ad178d52a9b3a2fd30a58ff57
Seonggil Jeong <63443366+seon99il@users.noreply.github.com>
2026-04-10 22:46:12 +0900

    Update job and DAG configurations for Spark image deployment (#29)
    * chore: deploy spark image fc6a4a8 [skip ci]
    
    * fix: update source and target table parameters in job and DAG configurations
    
    * chore: deploy spark image 9b104b9 [skip ci]
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- include/spark/jobs/update_repo_metrics_hourly/load_oltp_gold_repo_metrics_hourly_to_staging/job.py
+++ include/spark/jobs/update_repo_metrics_hourly/load_oltp_gold_repo_metrics_hourly_to_staging/job.py
@@ -6,20 +6,19 @@ from include.spark.utils.arg_parse_utils import parse_required_args
 from include.spark.utils.condition_utils import get_ingested_at_between_condition
 from include.spark.utils.jdbc_utils import get_jdbc_config
 
-source_gold_repo_metrics_table_name = "nessie.gitsight.gold.repo_metrics_hourly"
-
 
 def load_oltp_gold_repo_metrics_hourly_to_staging_job(
     *,
     spark: SparkSession,
     data_interval_start,
     data_interval_end,
-    staging_table_name,
+    source_table_name,
+    target_table_name,
 ):
     start_ts = pendulum.parse(data_interval_start).start_of("hour")
     end_ts = pendulum.parse(data_interval_end).start_of("hour")
 
-    source_df = spark.read.table(source_gold_repo_metrics_table_name).where(
+    source_df = spark.read.table(source_table_name).where(
         get_ingested_at_between_condition(start_ts, end_ts)
     )
 
@@ -29,7 +28,7 @@ def load_oltp_gold_repo_metrics_hourly_to_staging_job(
         source_df.coalesce(1)
         .write.format("jdbc")
         .option("url", jdbc_config.url)
-        .option("dbtable", staging_table_name)
+        .option("dbtable", target_table_name)
         .option("user", jdbc_config.user)
         .option("password", jdbc_config.password)
         .option("driver", jdbc_config.driver)
@@ -45,12 +44,18 @@ if __name__ == "__main__":
         "MergeToOltpGoldRepoMetricsHourlyJob"
     )
     args = parse_required_args(
-        ["data_interval_start", "data_interval_end", "target_table_name"]
+        [
+            "data_interval_start",
+            "data_interval_end",
+            "target_table_name",
+            "source_table_name",
+        ]
     )
 
     load_oltp_gold_repo_metrics_hourly_to_staging_job(
         spark=spark_session,
         data_interval_start=args.data_interval_start,
         data_interval_end=args.data_interval_end,
-        staging_table_name=args.target_table_name,
+        source_table_name=args.source_table_name,
+        target_table_name=args.target_table_name,
     )
